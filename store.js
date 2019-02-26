18fdc145e411bb5f18fbc4a78b0f5d5427ed986c
han_feng <han_feng@foxmail.com>
2019-02-26 21:58:52 +0800

    完善vuex存储策略

--- store.js
+++ store.js
@@ -1,13 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import vuexAlong from './vuex-along'
-
 // 初始化 Vuex
 Vue.use(Vuex)
-vuexAlong.onlySession(true)
-vuexAlong.watchSession(['session'], true)
 
-export default new Vuex.Store({
-  plugins: [vuexAlong]
-})
+export default new Vuex.Store()
